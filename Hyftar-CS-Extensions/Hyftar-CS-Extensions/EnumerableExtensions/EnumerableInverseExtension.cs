5e2bbe9ad5c930b71afa642ec683f6e2429bf105
Hyftar <landry_simon@pm.me>
2024-08-25 13:59:59 -0400

    Add `Select` extension and Inverse extensions

---
+++ Hyftar-CS-Extensions/Hyftar-CS-Extensions/EnumerableExtensions/EnumerableInverseExtension.cs
@@ -0,0 +1,17 @@
+﻿namespace HyftarCSExtensions.EnumerableExtensions;
+
+public static class EnumerableInverseExtension
+{
+    public static bool None<T>(this IEnumerable<T> source) => !source.Any();
+
+    public static bool None<T>(this IEnumerable<T> source, Func<T, bool> predicate) => !source.Any(predicate);
+
+    /// <summary>
+    /// Returns an <see cref="IEnumerable{T}"/> without the elements where the <paramref name="predicate"/> returned true
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="source"></param>
+    /// <param name="predicate"></param>
+    /// <returns>Enumerable without the elements where the predicate returned true</returns>
+    public static IEnumerable<T> Reject<T>(this IEnumerable<T> source, Func<T, bool> predicate) => source.Where(x => !predicate(x));
+}
---
+++ Hyftar-CS-Extensions/Hyftar-CS-Extensions/EnumerableExtensions/EnumerableTupleSelectExtension.cs
@@ -0,0 +1,19 @@
+﻿namespace HyftarCSExtensions.EnumerableExtensions;
+
+public static class EnumerableTupleSelectExtension
+{
+    public static IEnumerable<TResult> Select<TItem1, TItem2, TResult>(this IEnumerable<(TItem1, TItem2)> source, Func<TItem1, TItem2, TResult> selector)
+    {
+        return source.Select(s => selector(s.Item1, s.Item2));
+    }
+
+    public static IEnumerable<TResult> Select<TItem1, TItem2, TItem3, TResult>(this IEnumerable<(TItem1, TItem2, TItem3)> source, Func<TItem1, TItem2, TResult> selector)
+    {
+        return source.Select(s => selector(s.Item1, s.Item2));
+    }
+
+    public static IEnumerable<TResult> Select<TItem1, TItem2, TItem3, TITem4, TResult>(this IEnumerable<(TItem1, TItem2, TItem3, TITem4)> source, Func<TItem1, TItem2, TResult> selector)
+    {
+        return source.Select(s => selector(s.Item1, s.Item2));
+    }
+}
